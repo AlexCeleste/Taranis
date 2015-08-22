0249ccf64aed113b61a6c3adf4396d4f8d0c7753
Alex Gilding <alexg.nvfp@gmail.com>
2015-08-22 07:27:19 +0100

    fixup error system

--- Error.scm
+++ Error.scm
@@ -3,46 +3,33 @@
 
 ;; These procedures set the command line flags to disable warnings, or to
 ;; turn warnings into full errors.
-(define (no-warning) (set! no-warn-flag #t))
-(define (w-error) (set! w-error-flag #t))
+(define (no-warnings) (set! emit-warning (lambda r #!void)))
+(define (warning->error) (set! emit-warning raise))
 
-;; These are the actual flags (cannot export mutable variables)
-(define no-warn-flag #f)
-(define w-error-flag #f)
+(define emit-warning display)
+
+;; generate a location for errors and warnings
+(define (location-msg file lno cno)
+  (if lno
+    (string-append " on line " (number->string lno)
+      (if cno (string-append ":" (number->string cno)) "")
+      (if file (string-append " of '" file "'") "") )
+    (if file (string-append " in '" file "'") "") ))
 
 ;; Lexer standard error message
-(define (lex-err lno file msg)
-  (if lno
-      (display (string-append "Scanner error on line " (number->string lno)
-                              " of file \"" file "\":\n"))
-      (if file
-          (display (string-append "Scanner error in file \"" file "\":\n"))
-          (display "Scanner error:\n")))
-  (display (string-append "    " msg "\n"))
-  (newline)
-  (raise 'scanner-error))
+(define (lex-err msg #!optional file lno cno)
+  (raise (cons 'scanner-error
+    (string-append "Error" (location-msg file lno cno) ":\n  " msg "\n") )))
 
 ;; Lexer standard warning message
-(define (lex-warn lno file msg)
-  (unless no-warn-flag                          ;Potentially disable this
-    (when w-error-flag (lex-err lno file msg))  ;Pass it to full err proc
+(define (lex-warn msg #!optional file lno cno)
+  (emit-warning
+    (string-append "Warning: " msg "\n" (location-msg file lno cno) "\n") ))
 
-    (display (string-append "Warning: " msg "\n"))
-    (if lno
-        (display (string-append " on line " (number->string lno)
-                                " of file \"" file "\":\n"))
-        (display (string-append " in file \"" file "\":\n")))
-    (newline)))
-
-;; Struct-Parser standard error message
-(define (parse-err lno file msg)
-  (if lno
-      (display (string-append "Parser error on line " (number->string lno)
-                              " of file \"" file "\":\n"))
-      (display (string-append "Parser error in file \"" file "\":\n")))
-  (display (string-append "    " msg "\n"))
-  (newline)
-  (raise 'parser-error))
+;; Parser standard error message
+(define (parse-err msg #!optional file lno cno)
+  (raise (cons 'parser-error
+    (string-append "Error" (location-msg file lno cno) ":\n  " msg "\n") )))
 
 ;; Struct-Parser scoping error message
 (define (parse-scope-err msg)
@@ -60,10 +47,5 @@
 
 ;; General warning message
 (define (general-warn msg)
-  (unless no-warn-flag
-    (when w-error-flag (general-err msg))
-
-    (display "General warning: \n")
-    (display msg)
-    (newline)))
-
+  (emit-warning
+    (string-append "General warning: \n" msg "\n") ))
